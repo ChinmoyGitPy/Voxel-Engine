9367792829875cbd6da24f259d828826ad2ea0f4
Chinmoy Majumder <chinmoymajumder269@gmail.com>
2026-07-18 18:17:32 +0530

    added main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame as py
+from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+import numpy as np
+import math
+from collections import defaultdict
+import random
+
+grass = py.image.load()
